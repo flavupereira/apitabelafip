9b0be0623bdba00c3d473b3b21a8df458901c2fd
flavupereira <flavupereira@gmail.com>
2024-05-10 20:10:15 -0300

    first commit

---
+++ src/main/java/br/com/flavio/apitabelafipe/ApitabelafipeApplication.java
@@ -0,0 +1,13 @@
+package br.com.flavio.apitabelafipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApitabelafipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApitabelafipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/flavio/apitabelafipe/ApitabelafipeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.flavio.apitabelafipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApitabelafipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
